1513928f040eb851e0aac73c480f2374f243b05f
makhmud <insite-it@yandex.ru>
2020-09-16 17:11:33 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+	return (
+		<div className="fullpage">
+			<header className="header">
+				<span className="logo">Trello Clone</span>
+			</header>
+		</div>
+	);
+}
+
+export default App;
